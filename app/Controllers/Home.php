9d5b4e4dbbb3e7c9761b1349a0019c62ba88dfd8
Asnaat <asnaat05112001@gmail.com>
2025-09-17 14:57:07 +0530

    template integrate

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -11,49 +11,21 @@ use Psr\Log\LoggerInterface;
 
 class Home extends BaseController
 {
-    protected $productdisplayModel;
-    protected $categories;
 
-    public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
+
+    // public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
+    // {
+    //     parent::initController($request, $response, $logger);
+
+    //     $this->productdisplayModel = new ProductDisplayModel();
+    //     $this->categories = $this->productdisplayModel->getAllCategoriesAndSub();
+    // }
+
+    public function index()
     {
-        parent::initController($request, $response, $logger);
 
-        $this->productdisplayModel = new ProductDisplayModel();
-        $this->categories = $this->productdisplayModel->getAllCategoriesAndSub();
+        $template = view('index');
+        return $template;
     }
-
-   public function index()
-{
-    $data['categories'] = $this->categories;
-    $data['title'] = 'Homepage';
-
-    $products = $this->productdisplayModel->getAllProducts();
-    $reviewModel = new \App\Models\ReviewModel();
-    $productIds = array_column($products, 'pr_Id');
-    $avgRatings = $reviewModel->getAverageRatingForProducts($productIds);
-    $ratingsMap = [];
-    foreach ($avgRatings as $rating) {
-        $ratingsMap[$rating['pr_Id']] = $rating['avg_rating'];
-    }
-    foreach ($products as &$product) {
-        $product['avg_rating'] = $ratingsMap[$product['pr_Id']] ?? 0;
-    }
-    $data['product'] = $products;
-    // Load theme
-    $themeModel = new \App\Models\Admin\Theme_Model();
-    $themes = $themeModel->fetchTheme();
-    if (!empty($themes)) {
-        $data['themes'] = $themes[0];
-    }
-    // Load views
-    $template  = view('common/header', $data);
-    $template .= view('banner');
-    $template .= view('category', $data);
-    $template .= view('top_products', $data);
-    $template .= view('footer_banner', $data);
-    $template .= view('common/footer', $data);
-
-    return $template;
-}
 
 }
--- app/Views/common/hader_phn.php
+++
@@ -1,9 +0,0 @@
-        <div class="col-6">
-    <div class="row">
-        <div class="col-12 callnow tel-ico">
-            <i class="bi bi-telephone-fill"></i>
-            <span>Call Us Now</span><br />
-            <small><?= esc( $admin_user['us_Phone']) ?></small>
-        </div>
-    </div>
-</div>
